d225493b64687c04c5d830084e010813eda76e28
omkhr <45929538+omkhr@users.noreply.github.com>
2019-08-30 22:33:21 +0530

    Add files via upload

--- Day1/OddEvenSum.java
+++ Day1/OddEvenSum.java
@@ -1,38 +1,36 @@
-package com.assignments;
+package com.basic_example;
 
 public class OddEvenSum {
-	public static void main(String[] args)
-	{
-		int lowerbound=1,upperbound=1000;
-		int sumOdd=0;
-		int sumEven=0;
-		int absDiff;
-		
-		
-		int number = lowerbound;
-		
-		while(number<=upperbound)
-		{
-			if(number % 2==0)
-			{
-				sumEven += number;
-			}
-			else
-			{
-				sumOdd +=number;
-			}
-			++number;
-		}
-		if(sumOdd > sumEven)
-		{
-			absDiff = sumOdd-sumEven;
-		}else
-		{
-			absDiff = sumEven -sumOdd;
-		}
-		System.out.println("The sum of odd numbers from"+lowerbound+ "to" +upperbound+"is:"+sumOdd);
-		System.out.println("The sum of even numbers from"+lowerbound+ "to" +upperbound+"is:"+sumEven);
-		System.out.println("The absolute difference between the two sums is:"+absDiff);
-	}
 
+	public static void main(String[] args) {
+
+		int lowerbound=1;
+		int upperbound=1000;
+		int odd=0;
+		int even=0;
+		int diff;
+		
+		int no=lowerbound;
+		while(no<=upperbound) {
+			if(no%2==0) {
+				even+=no;
+			}
+			else {
+				odd+=no;
+			}
+			++no;
+		}
+		
+		if(odd>even) {
+			diff=odd-even;
+		}
+		else {
+			diff=even-odd;
+		}
+		
+		System.out.println("The Sum Of Odd No.From " +lowerbound+" To "+upperbound+" is:"+odd);
+		System.out.println("The Sum Of Even No.From "+lowerbound+" To "+upperbound+" is:"+even);
+		System.out.println("The Difference Between two sum is:"+diff);
+	}
+ 
 }
